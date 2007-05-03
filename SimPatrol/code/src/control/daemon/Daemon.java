94757077dd8d1f51ce57da494eb25d28e43c88f8
luiz.josue <luiz.josue@df1337f7-3e2b-0410-a383-03fb424f4498>
2007-05-03 02:45:51 +0000

    Added the package simpatrol.

--- SimPatrol/code/src/control/daemon/Daemon.java
+++
@@ -1,7 +0,0 @@
-package control.daemon;
-
-/**
- * @model.uin <code>design:node:::drykaf17vaioywpcl2h</code>
- */
-public abstract class Daemon {
-}
--- SimPatrol/code/src/control/requisition/Requisition.java
+++
@@ -1,9 +0,0 @@
-package control.requisition;
-
-import model.interfaces.XMLable;
-
-/**
- * @model.uin <code>design:node:::977fwf17vcg2cbz4ofu</code>
- */
-public abstract class Requisition implements XMLable {
-}
