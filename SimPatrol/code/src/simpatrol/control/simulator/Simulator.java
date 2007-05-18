53a471406beae6dc15d7fe96aeea3466bec3b1b2
dhenriques <dhenriques@df1337f7-3e2b-0410-a383-03fb424f4498>
2007-05-18 19:47:30 +0000

    Complementação da arquitetura do sistema: Simulador e suas associações com daemons, graph e societies.

--- SimPatrol/code/src/simpatrol/control/simulator/Simulator.java
+++ SimPatrol/code/src/simpatrol/control/simulator/Simulator.java
@@ -2,7 +2,12 @@ package simpatrol.control.simulator;
 
 import java.util.Collection;
 
+import simpatrol.control.daemon.ActionDaemon;
+import simpatrol.control.daemon.AnalysisReportDaemon;
+import simpatrol.control.daemon.PerceptionDaemon;
+import simpatrol.control.daemon.SimulationLogDaemon;
 import simpatrol.model.agent.Society;
+import simpatrol.model.graph.Graph;
 
 /**
  * @model.uin <code>design:node:::5fdg2f17vcpxt33cmgu</code>
@@ -13,6 +18,16 @@ public abstract class Simulator {
 	 * @model.uin <code>design:node:::hyht6f17ujey8gc0qlv</code>
 	 */
 	public Collection<Society> society;
+	
+	public Graph graph;
+	
+	public Collection<PerceptionDaemon> perceptionDaemon;
+	
+	public Collection<ActionDaemon> actionDaemon;
+	
+	public AnalysisReportDaemon analysisReportDaemon;
+	
+	public SimulationLogDaemon simulationLogDaemon;
 
 	/**
 	 * @model.uin <code>design:node:::co9mf17vcpxt-qsrxax:5fdg2f17vcpxt33cmgu</code>
