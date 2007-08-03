e650d51fbedbf1cd4435261e08277045753d3b22
luiz.josue <luiz.josue@df1337f7-3e2b-0410-a383-03fb424f4498>
2007-08-03 01:09:58 +0000

    Log information moved to the aspect ActionDaemonLogger.

--- SimPatrol/code/branch_1/src/control/daemon/ActionDaemon.java
+++ SimPatrol/code/branch_1/src/control/daemon/ActionDaemon.java
@@ -6,37 +6,39 @@ package control.daemon;
 /* Imported classes and/or interfaces. */
 import model.agent.Agent;
 
-/** Implements the daemons of SimPatrol that attend
- *  an agent's intentions of actions. */
+/**
+ * Implements the daemons of SimPatrol that attend an agent's intentions of
+ * actions.
+ */
 public final class ActionDaemon extends AgentDaemon {
 	/* Attributes. */
 	/* Methods. */
-	/** Constructor.
+	/**
+	 * Constructor.
 	 * 
-	 * 	Doesn't initiate its own connection, as it will be shared with a
-	 *  PerceptionDaemon object. So the connection must be set by the
-	 *  setConenction() method.
-	 *  @see PerceptionDaemon
-	 *
-	 *  @param thread_name The name of the thread of the daemon.
-	 *  @param agent The agent whose intentions are attended. */
+	 * Doesn't initiate its own connection, as it will be shared with a
+	 * PerceptionDaemon object. So the connection must be set by the
+	 * setConenction() method.
+	 * 
+	 * @see PerceptionDaemon
+	 * 
+	 * @param thread_name
+	 *            The name of the thread of the daemon.
+	 * @param agent
+	 *            The agent whose intentions are attended.
+	 */
 	public ActionDaemon(String thread_name, Agent agent) {
 		super(thread_name, agent);
 	}
-	
+
 	/** Indicates that the daemon must stop working. */
 	public void stopWorking() {
 		super.stopWorking();
-		
-		// screen message
-		System.out.println("[SimPatrol.ActionDaemon(" + this.agent.getObjectId() + ")]: Stopped working.");		
 	}
-	
+
 	public void run() {
-		// screen message
-		System.out.println("[SimPatrol.ActionDaemon(" + this.agent.getObjectId() + ")]: Listening to an intention...");
-		
-		while(!this.stop_working);
+		while (!this.stop_working)
+			;
 		// TODO implementar!!!
 	}
 }
