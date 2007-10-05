54317a22bcffceaeb411efdc300964528b7c5056
dhenriques <dhenriques@df1337f7-3e2b-0410-a383-03fb424f4498>
2007-10-05 14:55:54 +0000

    Initial import.

---
+++ SimPatrol Environment Editor/limitation/LimitationJPanel.java
@@ -0,0 +1,17 @@
+/* LimitationJPanel.java */
+
+/** The package of this class. */
+package limitation;
+
+/* Imported classes and/or interfaces. */
+import javax.swing.JPanel;
+import model.limitation.Limitation;
+
+/** Implements the GUI panel able to configure
+ *  Limitation objects.
+ *  
+ *  @see Limitation */
+public abstract class LimitationJPanel extends JPanel {
+	/** Returns the configured Limitation object. */
+	public abstract Limitation getLimitation();
+}
