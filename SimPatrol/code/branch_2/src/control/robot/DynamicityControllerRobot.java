fa0164beae538a02fbc796bb5c7570305f36bbb3
luiz.josue <luiz.josue@df1337f7-3e2b-0410-a383-03fb424f4498>
2007-10-27 22:01:55 +0000

    Code refactored after the inclusion of logging aspects

--- SimPatrol/code/branch_2/src/control/robot/DynamicityControllerRobot.java
+++ SimPatrol/code/branch_2/src/control/robot/DynamicityControllerRobot.java
@@ -7,65 +7,72 @@ package control.robot;
 import control.simulator.RealTimeSimulator;
 import model.interfaces.Dynamic;
 
-/** Implements the robots that assure dynamic behaviour
- *  to the dynamic objects of the simulation.
- *  
- *  Used by real time simulators.
- *  
- *  @see RealTimeSimulator */
+/**
+ * Implements the robots that assure dynamic behaviour to the dynamic objects of
+ * the simulation.
+ * 
+ * Used by real time simulators.
+ * 
+ * @see RealTimeSimulator
+ */
 public final class DynamicityControllerRobot extends Robot {
 	/* Attributes. */
 	/** The dynamic object to be controlled. */
 	private final Dynamic OBJECT;
-	
+
 	/* Methods. */
-	/** Constructor.
+	/**
+	 * Constructor.
 	 * 
-	 *  @param clock_thread_name The name of the thread of the clock of this robot.
-	 *  @param object The dynamic object to be controlled. */
+	 * @param clock_thread_name
+	 *            The name of the thread of the clock of this robot.
+	 * @param object
+	 *            The dynamic object to be controlled.
+	 */
 	public DynamicityControllerRobot(String clock_thread_name, Dynamic object) {
 		super(clock_thread_name);
 		this.OBJECT = object;
 	}
-	
+
 	public void act(int time_gap) {
-		synchronized(simulator) {
+		synchronized (simulator) {
 			simulator.getState();
-			
-			for(int i = 0; i < time_gap; i++) {		
+
+			for (int i = 0; i < time_gap; i++) {
 				// if the dynamic object is enabled
-				if(this.OBJECT.isEnabled()) {			
+				if (this.OBJECT.isEnabled()) {
 					// atualizes the enabling tpd
 					this.OBJECT.getEnablingTPD().nextBoolean();
-				
+
 					// verifies if the object must be disabled now
-					if(this.OBJECT.getDisablingTPD().nextBoolean())
+					if (this.OBJECT.getDisablingTPD().nextBoolean())
 						this.OBJECT.setIsEnabled(false);
 				}
 				// else
 				else {
 					// verifies if the object must be enabled now
-					if(this.OBJECT.getEnablingTPD().nextBoolean())
+					if (this.OBJECT.getEnablingTPD().nextBoolean())
 						this.OBJECT.setIsEnabled(true);
-					
+
 					// atualizes the disabling tpd
 					this.OBJECT.getDisablingTPD().nextBoolean();
 				}
 			}
-		}		
+		}
 	}
-	
+
 	public void start() {
 		super.start();
-		
-		// screen message
-		System.out.println("[SimPatrol.DynamicityRobot(" + this.OBJECT + ")]: Started working.");
 	}
-	
+
 	public void stopWorking() {
 		super.stopWorking();
-		
-		// screen message
-		System.out.println("[SimPatrol.DynamicityRobot(" + this.OBJECT + ")]: Stopped working.");
+	}
+
+	/**
+	 * @return The current Dynamic OBJECT
+	 */
+	public Dynamic getOBJECT() {
+		return OBJECT;
 	}
 }
