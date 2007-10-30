a345e33518385b44cfcba00fba4ef26d5b17ea2a
dhenriques <dhenriques@df1337f7-3e2b-0410-a383-03fb424f4498>
2007-10-30 14:12:49 +0000

    Initial import.

---
+++ SimPatrol Clients/src/reactive_with_flags/RealTimeReactiveWithFlagsAgent.java
@@ -0,0 +1,25 @@
+/* RealTimeReactiveWithFlagsAgent.java */
+
+/* The package of this class. */
+package reactive_with_flags;
+
+/* Imported classes and/or interfaces. */
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import util.net.UDPClientConnection;
+
+/** Implements the reactive with flags agents, as it is described
+ *  in the work of [MACHADO, 2002], for a real time simulator. */
+public class RealTimeReactiveWithFlagsAgent extends ReactiveWithFlagsAgent {
+	/* Methods. */
+	/** Constructor.
+	 * 
+	 *  @param remote_socket_address The IP address of the SimPatrol server.
+	 *  @param remote_socket_number The number of the socket that the server listens to, related to this agent. 
+	 *  @throws UnknownHostException 
+	 *  @throws SocketException */
+	public RealTimeReactiveWithFlagsAgent(String remote_socket_address, int remote_socket_number) throws SocketException, UnknownHostException {
+		super();
+		this.connection = new UDPClientConnection(remote_socket_address, remote_socket_number);
+	}
+}
