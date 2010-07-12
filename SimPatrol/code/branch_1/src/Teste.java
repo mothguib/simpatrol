4a8cd374311faa51208d459bed91fc8d400aa22f
valmirse <valmirse@df1337f7-3e2b-0410-a383-03fb424f4498>
2010-07-12 13:59:32 +0000

    Moving branch1 on the thunk to the right place

--- SimPatrol/code/branch_1/src/Teste.java
+++
@@ -1,8 +0,0 @@
-import java.net.SocketException;
-import control.simulator.RealTimeSimulator;
-
-public class Teste {
-	public static void main(String[] args) throws SocketException {		
-		new RealTimeSimulator(5000, 250);
-	}
-}
--- SimPatrol/code/branch_1/src/model/action/ActionTypes.java
+++
@@ -1,5 +0,0 @@
-package model.action;
-
-public class ActionTypes {
-
-}
