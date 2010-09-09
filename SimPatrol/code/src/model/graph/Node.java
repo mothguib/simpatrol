8eab0f7cbefc4f38fd1facf08a437793af2003ef
xenosender <xenosender@df1337f7-3e2b-0410-a383-03fb424f4498>
2010-09-09 17:13:47 +0000

    corrected node.getConnectingOutEdges and node.getConnectingInEdges

--- SimPatrol/code/src/model/graph/Node.java
+++ SimPatrol/code/src/model/graph/Node.java
@@ -426,7 +426,7 @@ public class Node implements XMLable, Visible {
 
 				// if the given Node is the source of the current edge,
 				// adds it to the answer
-				if (isSourceOf(current_edge))
+				if (node.isSourceOf(current_edge))
 					shared_edges.add(current_edge);
 			}
 		}
@@ -462,7 +462,7 @@ public class Node implements XMLable, Visible {
 
 				// if the given Node is the collector of the current edge,
 				// adds it to the answer
-				if(isTargetOf(current_edge))
+				if(node.isTargetOf(current_edge))
 					shared_edges.add(current_edge);
 			}
 		}
