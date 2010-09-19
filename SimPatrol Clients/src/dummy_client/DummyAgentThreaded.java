469eebaec5ed700bee3e6602f7d2a7a8e3838553
prof.pablo.sampaio <prof.pablo.sampaio@df1337f7-3e2b-0410-a383-03fb424f4498>
2010-09-19 04:09:36 +0000

    Issue 15 finished.

---
+++ SimPatrol Clients/src/dummy_client/DummyAgentThreaded.java
@@ -0,0 +1,94 @@
+package dummy_client;
+
+
+/**
+ * A simple agent that just goes back and forth between two nodes. 
+ * This is the "threaded version": each agent is a thread. It is implemented
+ * as a subclass of the "listener version" only to reuse code.
+ *  
+ * @author Pablo Sampaio
+ */
+public class DummyAgentThreaded extends DummyAgent implements Runnable {
+	
+
+	public DummyAgentThreaded(String agentId, TcpConnection con, String currentNode, String neighborNode) {
+		super(agentId, con, currentNode, neighborNode);
+	}
+	
+	
+	@Override
+	public void startWorking() {
+		connection.start();	
+		working = true;
+		
+		Thread thread = new Thread(this);
+		thread.start();
+	}	
+
+	@Override
+	public void stopWorking() {
+		asyncPrint("Preparing to finish..");
+		working = false;
+		connection.stopWorking();
+	}
+	
+	@Override
+	public void start() {
+		// not used
+	}
+	
+	@Override
+	public void update() {
+		// not used
+	}
+	
+	/**
+	 * Main method of this agent, where his behavior is implemented.
+	 */
+	@Override
+	public void run() {
+	
+		try {
+
+			while (working) {
+				visitAndGoToNextNode();
+				waitForArrivalMessage();				
+			}
+
+		} catch (Exception exc) {
+			asyncPrint("Error");
+			working = false;
+			exc.printStackTrace();
+			
+		}
+		
+		asyncPrint("Agent's thread finished");
+	}
+	
+	
+	/**
+	 * Waits the agent to arrive at his destiny node. Only returns when
+	 * the agent arrives. 
+	 */
+	private void waitForArrivalMessage() {
+		String[] messages;
+		boolean agentArrived = false;
+		int i;
+		
+		messages = connection.getBufferAndFlush();
+
+		while (!agentArrived && this.working) {
+			syncPrint("Waiting agent to arrive at node \"" + nodes[nextNode] + "\"");
+			
+			messages = connection.getBufferAndFlush();
+			
+			i = 0;
+			while (i < messages.length && !agentArrived) {
+				agentArrived = isArrivalMessage(messages[i]);
+				i ++;
+			}
+		}
+		
+	}
+	
+}
